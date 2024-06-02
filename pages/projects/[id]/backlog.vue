bbff9c1c8299071bde7e7fd630a5ede567c8df79
maarten-mald-digital <maartenstolk72@gmail.com>
2024-06-02 14:47:23 +0200

    backup

--- pages/projects/[id]/backlog.vue
+++ pages/projects/[id]/backlog.vue
@@ -6,24 +6,94 @@
 			</div>
 
 			<div class="col-md-9">
-				<h1 class="my-3">Backlog: {{ project.title }}</h1>
-				<NuxtLink :to="{ name: 'projects' }">
-					<button>Return</button>
-				</NuxtLink>
+				<div>
+					<h1 class="my-3">Backlog: {{ project.title }}</h1>
+					<NuxtLink :to="{ name: 'projects' }">
+						<button>Return</button>
+					</NuxtLink>
+					<hr />
+				</div>
+
+				<!-- <pre>{{ project }}</pre> -->
+				<!-- <pre>{{ sprints }}</pre> -->
+				<!-- <pre>{{ backlog }}</pre> -->
+
+				<div>
+					<!-- Sprints -->
+					<div class="sprint mt-3" v-for="sprint in sprints">
+						<h3>{{ sprint.title }}</h3>
+						<draggable
+							v-model="sprint.tasks"
+							group="tasks"
+							class="task-list"
+							@end="onEnd"
+							itemKey="name"
+						>
+							<template #item="{ element, index }">
+								<div class="task-item">
+									{{ element.title }}
+								</div>
+							</template>
+						</draggable>
+					</div>
+
+					<!-- Backlog -->
+					<div class="backlog mt-5">
+						<h3>Backlog</h3>
+						<draggable
+							v-model="backlog.tasks"
+							group="tasks"
+							class="task-list"
+							@end="onEnd"
+							itemKey="name"
+						>
+							<template #item="{ element, index }">
+								<div class="task-item">
+									{{ element.title }}
+								</div>
+							</template>
+						</draggable>
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
 </template>
 
 <script lang="ts" setup>
+import draggable from 'vuedraggable';
+
 const route = useRoute();
 const projectId = Number(route.params.id);
 const project = ref<any>(null);
+
+const sprints = ref<any>();
+const backlog = ref<any>();
 
 await useAsyncGql({
 	operation: 'GetProject',
 	variables: { id: projectId },
 }).then((response) => {
 	project.value = response.data.value.project;
+
+	console.log(project.value);
+
+	sprints.value = project.value.sprints.map((sprint) => ({
+		...sprint,
+		tasks: project.value.tasks.filter(
+			(task) => task.sprint_id === sprint.id
+		),
+	}));
+
+	backlog.value = {
+		tasks: project.value.tasks.filter((task) => !task.sprint_id),
+	};
+
+	// console.log(sprints.value);
 });
+
+function onEnd(event: any) {
+	// Handle the end of drag-and-drop event if needed
+	console.log('Drag ended:', event);
+}
 </script>
--- pages/projects/[id]/board.vue
+++ pages/projects/[id]/board.vue
@@ -1,21 +1,87 @@
 <template>
 	<section>
-		<div class="row">
-			<div class="col-md-3">
-				<project-sidebar :project="project" />
-			</div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-3">
+					<project-sidebar :project="project" />
+				</div>
 
-			<div class="col-md-9">
-				<h1 class="my-3">Board: {{ project.title }}</h1>
-				<NuxtLink :to="{ name: 'projects' }">
-					<button>Return</button>
-				</NuxtLink>
+				<div class="col-md-9">
+					<div>
+						<h1 class="my-3">Board: {{ project.title }}</h1>
+						<NuxtLink :to="{ name: 'projects' }">
+							<button>Return</button>
+						</NuxtLink>
+						<hr />
+					</div>
+
+					<div class="boards kanban">
+						<div class="row">
+							<div class="col-6">
+								<h2>To-Do</h2>
+								<draggable
+									v-model="todoList"
+									group="tasks"
+									class="list"
+									@end="onEnd"
+									itemKey="name"
+								>
+									<template #item="{ element, index }">
+										<div class="list-item">
+											{{ element.title }}
+										</div>
+									</template>
+								</draggable>
+							</div>
+
+							<div class="col-6">
+								<h2>Done</h2>
+								<draggable
+									v-model="doneList"
+									group="tasks"
+									class="list"
+									@end="onEnd"
+									itemKey="name"
+								>
+									<template #item="{ element, index }">
+										<div class="list-item">
+											{{ element.title }}
+										</div>
+									</template>
+								</draggable>
+							</div>
+						</div>
+					</div>
+
+					<div class="boards kanban">
+						<div class="row">
+							<div class="col-3" v-for="lane in lanes">
+								<h2>{{ lane.title }}</h2>
+								<draggable
+									v-model="lane.tasks"
+									group="tasks"
+									class="list"
+									@end="onEnd"
+									itemKey="name"
+								>
+									<template #item="{ element, index }">
+										<div class="list-item">
+											{{ element.title }}
+										</div>
+									</template>
+								</draggable>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
 </template>
 
 <script lang="ts" setup>
+import draggable from 'vuedraggable';
+
 const route = useRoute();
 const projectId = Number(route.params.id);
 const project = ref<any>(null);
@@ -26,4 +92,48 @@ await useAsyncGql({
 }).then((response) => {
 	project.value = response.data.value.project;
 });
+
+// Draggable test
+interface Task {
+	id: number;
+	title: string;
+}
+
+interface Lane {
+	id: number;
+	title: string;
+	tasks: Task[];
+}
+
+const todoList = ref<Task[]>([
+	{ id: 1, title: 'Task 1' },
+	{ id: 2, title: 'Task 2' },
+	{ id: 3, title: 'Task 3' },
+]);
+
+const doneList = ref<Task[]>([]);
+
+// const board = ref<any[]>([{
+// 	lanes:
+// }]);
+
+const lanes = ref<Lane[]>([
+	{
+		id: 1,
+		title: 'to do',
+		tasks: [
+			{ id: 1, title: 'Task 1' },
+			{ id: 2, title: 'Task 2' },
+			{ id: 3, title: 'Task 3' },
+		],
+	},
+	{ id: 2, title: 'in progress', tasks: [{ id: 4, title: 'Task 4' }] },
+	{ id: 3, title: 'test', tasks: [] },
+	{ id: 4, title: 'done', tasks: [] },
+]);
+
+function onEnd(event: any) {
+	// Handle the end of drag-and-drop event if needed
+	console.log('Drag ended:', event);
+}
 </script>
